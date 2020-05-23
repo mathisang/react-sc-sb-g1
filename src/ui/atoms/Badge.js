0e05dfc5d71ea04e5b3683b717107c86745b99b0
Mathis Angibaud <m.angibaudmathis@gmail.com>
2020-05-23 14:32:49 +0200

    Commentaires explications

--- src/ui/atoms/Badge.js
+++ src/ui/atoms/Badge.js
@@ -3,11 +3,13 @@ import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import Colors from "../particles/Colors";
 
+// pq purple = purple et pareil grey
 export const colors = {
     purple: "purple",
     grey: "grey"
 };
 
+// Styled component, defini un style a une balise créer
 const BadgeWrapper = styled.span`
   background: ${props => (props.color === colors.purple ? Colors.brand_primary : Colors.neutral_grey)};
   margin-right: 8px;
@@ -18,16 +20,20 @@ const BadgeWrapper = styled.span`
   font-size: 12px;
 `;
 
+// Const de l'affichage + set des props pour les récupérer, et nom balise en fonction de styledcomponent
 const Badge = props => {
     const { text, color } = props;
     return <BadgeWrapper color={color}>{text}</BadgeWrapper>;
 };
 
+
+// PROPTYPES Déclaration du type et obligatoire ou non (erreur console log si pas bon)
 Badge.propTypes = {
     color: PropTypes.oneOf(Object.keys(colors)),
     text: PropTypes.string
 };
 
+// Value par défaut
 Badge.defaultProps = {
     color: colors.grey
 };
